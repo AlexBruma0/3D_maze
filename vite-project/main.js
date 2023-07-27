5b37dde6ce87d3cf11a6d664677fc0b44cc9f720
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-07-27 11:15:25 -0700

    new stuff

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,66 +1,87 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function main() {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	const canvas = document.querySelector( '#c' );
+	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
+	const fov = 70;
+	const near = 0.1;
+	const far = 100;
+	const camera = new THREE.PerspectiveCamera( fov, window.innerWidth / window.innerHeight, near, far );
+	camera.position.set( 0, 10, 20 );
 
-drawCube(1,1,1,0x00ff00)
-drawCube(1,0.5,5,0xff0000)
+	const controls = new OrbitControls( camera, canvas );
+	controls.target.set( 0, 5, 0 );
+  controls.enableDamping = false;
+	controls.update();
 
-function drawCube(width, height, depth, color){
-  const geometry = new THREE.BoxGeometry( width, height, depth );
-  const material = new THREE.MeshBasicMaterial( { color: color } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+	const scene = new THREE.Scene();
+	scene.background = new THREE.Color( 'white' );
+  //floor
+
+	{
+		const planeSize = 400;
+		const loader = new THREE.TextureLoader();
+		const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+		texture.magFilter = THREE.NearestFilter;
+		texture.colorSpace = THREE.SRGBColorSpace;
+		const repeats = planeSize / 2;
+		texture.repeat.set( repeats, repeats );
+
+		const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
+		const planeMat = new THREE.MeshPhongMaterial( {
+			map: texture,
+			side: THREE.DoubleSide,
+		} );
+		const mesh = new THREE.Mesh( planeGeo, planeMat );
+		mesh.rotation.x = Math.PI * - .5;
+		scene.add( mesh );
+
+	}
+  //cube
+	{
+
+		const cubeSize = 4;
+		const cubeGeo = new THREE.BoxGeometry( cubeSize/4, cubeSize*2, cubeSize*2 );
+		const cubeMat = new THREE.MeshPhongMaterial( { color: '#8AC' } );
+		const mesh = new THREE.Mesh( cubeGeo, cubeMat );
+		mesh.position.set( cubeSize + 1, cubeSize / 2, 0 );
+		scene.add( mesh );
+
+	}
+  //sphere
+	{
+
+		const sphereRadius = 3;
+		const sphereWidthDivisions = 32;
+		const sphereHeightDivisions = 16;
+		const sphereGeo = new THREE.SphereGeometry( sphereRadius, sphereWidthDivisions, sphereHeightDivisions );
+		const sphereMat = new THREE.MeshPhongMaterial( { color: '#CA8' } );
+		const mesh = new THREE.Mesh( sphereGeo, sphereMat );
+		mesh.position.set( - sphereRadius - 1, sphereRadius + 2, 0 );
+		scene.add( mesh );
+
+	}
+  //light
+	{
+
+		const color = 0xFFFFFF;
+		const intensity = 2;
+		const light = new THREE.AmbientLight( color, intensity );
+		scene.add( light );
+
+	}
+
+	function render() {
+		renderer.render( scene, camera );
+		requestAnimationFrame( render );
+	}
+	requestAnimationFrame( render );
 }
 
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	renderer.render( scene, camera );
-}
-const move_speed = 0.5;
-
-document.addEventListener('keydown', (event) =>{
-  moveCameraOnKey(event, 'w', () => camera.translateZ(-move_speed))
-  moveCameraOnKey(event, 's', () => camera.translateZ(move_speed))
-  moveCameraOnKey(event, 'a', () => camera.translateX(-move_speed))
-  moveCameraOnKey(event, 'd', () => camera.translateX(move_speed))
-})
-
-document.addEventListener('mousemove', (event) =>{
-  rotateCameraOnMouseMove(event)
-})
-
-function moveCameraOnKey(event, key, move){
-  if(event.key === key){
-    move()
-  }
-}
-const rotate_speed = 0.05
-
-function rotateCameraOnMouseMove(event){
-  if(event.which === 3){
-    if(event.movementX > 0){
-      camera.rotateY(-rotate_speed)
-    }
-    if(event.movementX < 0){
-      camera.rotateY(rotate_speed)
-    }
-    if(event.movementY > 0){
-      camera.rotateX(-rotate_speed)
-    }
-    if(event.movementY < 0){
-      camera.rotateX(rotate_speed)
-    }
-    
-  }
-}
-
-animate();
+main();
