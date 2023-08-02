39169b390db7e0a66613bd6ff600c80dd7b5f6bb
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-08-02 11:19:23 -0700

    texture

--- vite-project/meshes.js
+++ vite-project/meshes.js
@@ -138,24 +138,28 @@ const walls = [
 export function meshes(scene) {
     floor(scene)
     for(var i = 0; i < walls.length; i++){
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load( "./me.png" );
+        texture.colorSpace = THREE.SRGBColorSpace;
+        var material;
+
         const cubeGeo = new THREE.BoxGeometry( walls[i].dimension[0], walls[i].dimension[1], walls[i].dimension[2] * unit_length );
-        const cubeMat = new THREE.MeshPhongMaterial( { color: '#000' } );
-        const mesh = new THREE.Mesh( cubeGeo, cubeMat );
-        mesh.position.set(walls[i].position[0]*unit_length, walls[i].position[1]*unit_length, -walls[i].position[2]*unit_length  );
+        if( i  == 2){
+             material = new THREE.MeshBasicMaterial( {
+                map: texture,
+            } );
+        }
+        else{
+            material = new THREE.MeshPhongMaterial( { color: '#000' } );
+        }
+        
+        const mesh = new THREE.Mesh( cubeGeo, material );
+        mesh.position.set(walls[i].position[0]*unit_length, walls[i].position[1]*unit_length + 4, -walls[i].position[2]*unit_length  );
         mesh.rotateY(walls[i].rotation)
-        console.log(mesh)
         scene.add( mesh );
     }
 }
-function cube(scene,x,y,z,width,height,depth){
-    const cubeGeo = new THREE.BoxGeometry( width, height, depth );
-    const cubeMat = new THREE.MeshPhongMaterial( { color: '#f00' } );
-    const mesh = new THREE.Mesh( cubeGeo, cubeMat );
-    mesh.position.set( x,y,z );
 
-    scene.add( mesh );
-    return mesh
-}
 function floor(scene){
     const planeSize = 400;
     const loader = new THREE.TextureLoader();
@@ -164,7 +168,7 @@ function floor(scene){
     texture.wrapT = THREE.RepeatWrapping;
     texture.magFilter = THREE.NearestFilter;
     texture.colorSpace = THREE.SRGBColorSpace;
-    const repeats = planeSize / 2;
+    const repeats = planeSize/2 ;
     texture.repeat.set( repeats, repeats );
 
     const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
